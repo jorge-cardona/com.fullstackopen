e5bbf60d906e40c8a5a9d3cdbdf7518af7df0bb1
Jorge Cardona <jorge@Jorges-MacBook-Pro.local>
2025-08-02 11:13:20 -0500

    notes, deploy to render

--- part2/collections/backend/index.js
+++ part2/collections/backend/index.js
@@ -1,7 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -90,7 +93,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
