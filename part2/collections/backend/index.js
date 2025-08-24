e3596429b22b6a864884dca9559688ab4c62cafd
Jorge Cardona <jorgecardonavargas@gmail.com>
2025-08-24 22:21:22 +0100

    notes, uibuild

--- part2/collections/backend/index.js
+++ part2/collections/backend/index.js
@@ -88,14 +88,14 @@ app.put('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    });
-  }
+  // if (!body.content) {
+  //   return response.status(400).json({
+  //     error: 'content missing'
+  //   });
+  // }
 
   const note = new Note({
     content: body.content,
@@ -104,7 +104,7 @@ app.post('/api/notes', (request, response) => {
 
   note.save().then(savedNote => {
     response.json(savedNote);
-  });
+  }).catch(error => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -118,6 +118,8 @@ const errorHandler = (error, request, response, next) => {
   
   if(error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
